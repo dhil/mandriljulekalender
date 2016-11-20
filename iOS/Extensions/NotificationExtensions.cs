0b4bc8eb16f54eba01b23b55847cbe2f33a51c5e
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2016-11-20 20:19:46 +0100

    iOS extension methods for converting an UILocalNotification to a MandrilNotification

--- iOS/Extensions/NotificationExtensions.cs
+++ iOS/Extensions/NotificationExtensions.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Diagnostics.Contracts;
+using UIKit;
+
 namespace Mandrilkalender.iOS
 {
-	public class NotificationExtensions
+	public static class NotificationExtensions
 	{
-		public NotificationExtensions()
+		public static MandrilNotification ToMandrilNotification(this UILocalNotification notification)
 		{
+			return new MandrilNotification
+			{
+				Title = notification.AlertTitle,
+				Content = notification.AlertBody,
+				FireDate = notification.FireDate.ToDateTime()
+			};
 		}
 	}
 }
